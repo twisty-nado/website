0370f48dc0d48ba2c44f741e099f66a9559fef0d
twisty-nado <99951556+twisty-nado@users.noreply.github.com>
2023-12-21 15:24:37 +0000

    Add files via upload

--- js.js
+++ js.js
@@ -47,3 +47,65 @@ function scrollto(w){
         document.querySelector(w).scrollIntoView();
     }
 }
+
+function checkBlur(){
+    var check = document.getElementById("blurCh")
+    if (check.checked){
+        document.documentElement.style.setProperty("--blur", "8px")
+    } else{
+        document.documentElement.style.setProperty("--blur", "0px")
+    }
+}
+
+function checkOpacity(){
+    var check = document.getElementById("opacityCh")
+    var check2 = document.getElementById("blurCh")
+    if (check.checked){
+        document.documentElement.style.setProperty("--opacity", ".6")
+        check2.disabled = false
+    } else{
+        document.documentElement.style.setProperty("--opacity", "1")
+        check2.checked = false
+        check2.disabled = true
+    }
+}
+
+function checkZoom(){
+    var check = document.getElementById("zoomCh")
+    if (check.checked){
+        document.documentElement.style.setProperty("--zoom", "105%")
+    } else{
+        document.documentElement.style.setProperty("--zoom", "100%")
+    }
+}
+
+function checkHover(){
+    var check = document.getElementById("hoverCh")
+    if (check.checked){
+        document.documentElement.style.setProperty("--hover", "#ffffff65")
+    } else{
+        document.documentElement.style.setProperty("--hover", "#00000065")
+    }
+}
+
+function downloadFiles(f){
+    if (f == "cbm"){
+        location.href = "https://github.com/twisty-nado/website/releases/download/downloads/cbm-1-0-2-b-c.exe"
+    } else if (f == "ttt"){
+        location.href = "https://github.com/twisty-nado/website/releases/download/downloads/tictactoe.exe"
+    }
+}
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    m = checkTime(m);
+    document.querySelector('.clock').innerHTML =  h + ":" + m;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+  }
